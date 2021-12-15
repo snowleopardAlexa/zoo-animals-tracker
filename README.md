b6bc7158818948ee786b00f97c292e2c1983c4cb
snowleopardAlexa <alexaslomski@gmail.com>
2021-12-14 22:42:05 -0500

    intial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+### Simple Notes Context Api app
+
+How to use Context API.
+
+
+
---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, { useState } from 'react';
+import './App.css';
+
+function App() {
+
+
+
+  return (
+    <div>
+
+    </div>
+  );
+}
+
+export default App;
