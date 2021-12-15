fa02990b0a00ffc4a6a3fbbb17513adb939abac2
snowleopardAlexa <alexaslomski@gmail.com>
2021-12-14 22:45:24 -0500

    add notes state

--- src/App.js
+++ src/App.js
@@ -3,13 +3,28 @@ import './App.css';
 
 function App() {
 
-
+  const [notes] = useState([
+    {
+      title: "First note",
+      description: "This is my first note",
+      done: false
+    }
+  ]);
 
   return (
     <div>
-
+      <h1>Notes</h1>
+      {notes.map(note => {
+        return (
+          <div>
+            <h1>{note.title}</h1>
+            <h3>{note.description}</h3>
+            <p>{note.done ? "done!" : "not done!"}</p>
+          </div>
+        );
+      })}
     </div>
   );
-}
+};
 
 export default App;
