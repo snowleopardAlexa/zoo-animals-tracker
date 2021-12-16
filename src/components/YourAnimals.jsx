ad5bd0de3a9c423409d9ff34703fec55b50fe1ae
snowleopardAlexa <alexaslomski@gmail.com>
2021-12-15 21:04:28 -0500

    add global context to youranimals, add total function

--- src/components/YourAnimals.jsx
+++ src/components/YourAnimals.jsx
@@ -1,10 +1,18 @@
-import React from 'react'
+import React, { useContext } from 'react';
+import { useContext } from 'react/cjs/react.development';
+import { GlobalContext } from '../context/GlobalState';
 
 function YourAnimals() {
+
+    const { animalTransfers } = useContext(GlobalContext);
+
+    const animals = animalTransfers.map(animalTransfer => animalTransfer.animal);
+    const total = animals.reduce((acc, item) => (acc += item) , 0).toFixed(2);
+
     return (
         <div>
             <h4>Your Animals</h4>
-            <h1 id="balance">10</h1>
+            <h1 id="balance">{total}</h1>
         </div>
     )
 }
