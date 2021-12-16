ab9a78629379970f5799a42cadac3d98dd107ecf
snowleopardAlexa <alexaslomski@gmail.com>
2021-12-15 20:42:08 -0500

    add context to animal transfer list and map over it

--- src/components/AnimalTransferList.jsx
+++ src/components/AnimalTransferList.jsx
@@ -1,10 +1,17 @@
-import React from 'react'
+import React, { useContext } from 'react';
+import { GlobalContext } from '../context/GlobalState';
 
 function AnimalTransferList() {
+
+    const { animalTransfers } = useContext(GlobalContext);
+
     return (
         <div>
             <h3>History</h3>
             <ul id="list" className="list">
+                {animalTransfers.map(animalTransfer => (<li className="minus">
+                    {animalTransfer.text} <span>-1</span><button className="delete-btn">x</button>
+                    </li>))}
                 Animal <span>Snow Leopard</span><button class="delete-btn">x</button>
             </ul>
         </div>
